91d43c4a8541a2b919ab9508d5e46107238c7d15
Ezra Sandzer-Bell <equilibriumarts@gmail.com>
2016-12-02 13:23:15 -0800

    clean up code layout

--- lib/words.rb
+++ lib/words.rb
@@ -1,25 +1,33 @@
 class Word
+
   @@all_words = []
+
   define_method(:initialize) do |word|
     @word = word
     @id = @@all_words.length + 1
     @definitions = []
   end
+
   define_method(:save) do
     @@all_words.push(self)
   end
+
   define_singleton_method(:all) do
     @@all_words
   end
+
   define_method(:get_word) do
     @word
   end
+
   define_singleton_method(:clear) do
     @@all_words = []
   end
+
   define_method(:id) do
     @id
   end
+
   define_singleton_method(:find) do |id|
     found_word = nil
     @@all_words.each do |word|
@@ -29,9 +37,11 @@ class Word
     end
     found_word
   end
+
   define_method(:definitions) do
     @definitions
   end
+
   define_method(:save_definition) do |definition|
     @definitions.push(definition)
   end
