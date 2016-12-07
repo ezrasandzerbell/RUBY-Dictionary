20b841fb8151532792f978ecde32b16cc4147fd1
ezra <equilibriumarts@gmail.com>
2016-12-06 18:54:37 -0800

    create and pass rspec testing part of speech method for definition

--- spec/definitions_spec.rb
+++ spec/definitions_spec.rb
@@ -8,4 +8,10 @@ describe(Definition) do
       expect(test_definition.definition).to(eq("to smile"))
     end
   end
+  describe('#pos') do
+    it('tests for parts of speech passed through argument') do
+      test_definition = Definition.new({:definition => "to smile", :pos => "verb"})
+      expect(test_definition.pos).to(eq("verb"))
+    end
+  end
 end
